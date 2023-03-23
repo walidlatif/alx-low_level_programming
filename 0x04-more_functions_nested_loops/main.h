7bff899d0bac6d691aa3cc5d8f7886f4c15312ce
walidlatif <walidlatif45@gmail.com>
2023-03-23 07:03:48 -0700

    3. The numbers speak for themselves

--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -1,6 +1,7 @@
 #ifndef MAIN_H
 #define MAIN_H
 
+int _putchar(char c);
 int _isupper(int c);
 int _isdigit(int c);
 int mul(int a, int b);
@@ -13,4 +14,3 @@ void print_square(int size);
 void print_triangle(int size);
 
 #endif
-
