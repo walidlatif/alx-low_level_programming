377378ce9f5b487cc2fb86fcb67e4f539d96da69
walidlatif <walidlatif45@gmail.com>
2023-03-27 07:44:15 -0700

    2. This report, by its very length, defends itself against the risk of being read

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,18 @@
+#include "main.h"
+#include <string.h>
+
+/**
+* _strlen - returns the length of a string
+*
+* @s: string passed
+*
+* Return: returns length of string passed
+*/
+
+int _strlen(char *s)
+{
+	int len;
+
+	len  = strlen(s);
+	return (len);
+}
