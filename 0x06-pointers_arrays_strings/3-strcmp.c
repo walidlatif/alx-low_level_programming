12b05fd163a81eb1148e65ac5e61fa5810fbdd2d
walidlatif <walidlatif45@gmail.com>
2023-03-29 15:41:23 -0700

    3. strcmp

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -1,19 +1,21 @@
+#include <string.h>
 #include "main.h"
-#include <stdio.h>
 
 /**
- * main - check the code
+ * _strcmp - compares two strings.
  *
- * Return: Always 0.
+ * @s2: string passed
+ *
+ * @s1: string passed
+ *
+ * Return: result.
  */
-int main(void)
-{
-    char s1[] = "Hello";
-    char s2[] = "World!";
 
-    printf("%d\n", _strcmp(s1, s2));
-    printf("%d\n", _strcmp(s2, s1));
-    printf("%d\n", _strcmp(s1, s1));
-    return (0);
+int _strcmp(char *s1, char *s2)
+{
+        int result;
+
+        result = strcmp(s1, s2);
+        return (result);
 }
 
