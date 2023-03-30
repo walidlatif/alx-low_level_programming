62a8441286ce10a52df1bb1581f6859232a0d310
walidlatif <walidlatif45@gmail.com>
2023-03-30 03:31:00 -0700

    5. Always look up

---
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,21 @@
+#include "main.h"
+#include <string.h>
+#include <ctype.h>
+/**
+ * string_toupper - changes all lowercase letters of a string to uppercase.
+ *
+ * @str: string passed
+ *
+ * Return: str
+ */
+
+char *string_toupper(char *str)
+{
+	long unsigned int i;
+
+	for (i = 0; i <= strlen(str); i++)
+	{
+		str[i] = toupper(str[i]);
+	}
+	return (str);
+}
