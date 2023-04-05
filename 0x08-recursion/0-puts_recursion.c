0d25e535f4e63f8d055dc6fc6c36c00c945f0466
walidlatif <walidlatif45@gmail.com>
2023-04-05 09:36:46 -0700

    0. She locked away a secret ...

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -3,10 +3,18 @@
 /**
 * _puts_recursion - prints a string.
 *@s: String Passed
-*Return: NULL
+*Return: Void
 */
 
 void _puts_recursion(char *s)
 {
-	puts(s);
+	if (*s == '\0')
+	{
+		_putchar('\n');
+	}
+	else
+	{
+		_putchar(*s);
+		_puts_recursion(s + 1);
+	}
 }
