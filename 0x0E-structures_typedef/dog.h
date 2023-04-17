d1e10f869a1d6cbc991ac8910367cfade1977e38
walidlatif <walidlatif45@gmail.com>
2023-04-17 05:03:23 -0700

    0. Poppy

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,14 @@
+/**
+ * struct dog - Short description
+ * @name: dog name
+ * @age: dog age
+ * @owner: dog owner
+ * Description: Longer description
+ */
+
+struct dog 
+{
+	char *name;
+	float age;
+	char *owner;
+};
