a0e5370954ace40882e4abe74e4bc17df9be7ed5
walidlatif <walidlatif45@gmail.com>
2023-07-04 10:45:38 -0700

    102

--- 0x13-more_singly_linked_lists/102-free_listint_safe.c
+++ 0x13-more_singly_linked_lists/102-free_listint_safe.c
@@ -1,31 +1,86 @@
 #include "lists.h"
 
 /**
- * free_listint_safe - Frees a listint_t linked list safely
- * @h: Pointer to pointer to the head of the list
+ * loop_listint_len - calculates the length of a loop in a linked list
+ * @head: pointer to the head of the linked list
  *
- * Return: Size of the list that was free'd
+ * Return: the number of nodes in the loop, or 0 if there is no loop
  */
-
-size_t free_listint_safe(listint_t **h)
+size_t loop_listint_len(const listint_t *head)
 {
-	listint_t *current = *h, *next = NULL;
-	size_t count = 0;
+	const listint_t *slow_ptr, *fast_ptr;
+	size_t length = 1;
 
-	while (current != NULL)
+	/* If the list is empty or has only one node, there is no loop */
+	if (head == NULL || head->next == NULL)
+		return (0);
+
+	slow_ptr = head->next;  /* tortoise pointer */
+	fast_ptr = head->next->next;  /* hare pointer */
+
+	/* Floyd's cycle-finding algorithm */
+	while (fast_ptr != NULL)
 	{
-		next = current->next;
-		free(current);
-		count++;
-
-		if (next >= current)
+		if (slow_ptr == fast_ptr)
 		{
-			*h = NULL;
-			break;
+			slow_ptr = head;
+			while (slow_ptr != fast_ptr)
+			{
+				length++;
+				slow_ptr = slow_ptr->next;
+				fast_ptr = fast_ptr->next;
+			}
+
+			slow_ptr = slow_ptr->next;
+			while (slow_ptr != fast_ptr)
+			{
+				length++;
+				slow_ptr = slow_ptr->next;
+			}
+
+			return (length);
 		}
 
-		current = next;
+		slow_ptr = slow_ptr->next;
+		fast_ptr = fast_ptr->next->next;
 	}
 
-	return (count);
+	return (0);
+}
+
+/**
+ * free_listint_safe - frees a listint_t list
+ * @h: double pointer to the head of the list
+ * Return: the size of the list that was free'd
+ */
+size_t free_listint_safe(listint_t **h)
+{
+	listint_t *tmp;
+	size_t length, i;
+
+	length = loop_listint_len(*h);
+
+	if (length == 0)
+	{
+		for (i = 0; *h != NULL; i++)
+		{
+			tmp = *h;
+			*h = (*h)->next;
+			free(tmp);
+		}
+	}
+	else
+	{
+		for (i = 0; i < length; i++)
+		{
+			tmp = *h;
+			*h = (*h)->next;
+			free(tmp);
+		}
+		*h = NULL;
+	}
+
+	h = NULL;
+
+	return (i);
 }
